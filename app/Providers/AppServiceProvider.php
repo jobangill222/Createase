4a326ee6f0b988c901eb196e5b5de027dcd89d04
Apple <apple@jobanpreet-singh.local>
2024-03-20 22:58:45 +0530

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,5 +36,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(env('APP_HTTPS')){
+            \URL::forceScheme('https');
+            $this->app(['request']->server->set('HTTPS' , 'on'));
+        }   
+
     }
 }
