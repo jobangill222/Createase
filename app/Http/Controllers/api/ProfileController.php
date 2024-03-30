d8f09f1bc7bc9817794fecf57bbab8a1bbc5bed9
Apple <apple@192.168.1.3>
2024-03-30 17:33:09 +0530

    correct name

--- app/Http/Controllers/api/ProfileController.php
+++ app/Http/Controllers/api/ProfileController.php
@@ -96,7 +96,7 @@ class ProfileController extends Controller
                 $image_data = [
                     'user_id' => $user->id,
                     'image' => basename($user->profile_pic),
-                    'is_active' => 1
+                    'is_active' => 0
                 ];
                 UserImage::create($image_data);
             }
